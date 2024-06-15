3cc4d34ed7a000e0ea5ad0afea398541835bbfb2
Botti Bao <botti.bao@studio.unibo.it>
2024-06-15 15:39:10 +0200

    Gradle

---
+++ app/src/main/java/App.java
@@ -0,0 +1,23 @@
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import data.DAOUtils;
+
+public final class App {
+
+    public static void main(String[] args) throws SQLException {
+
+        var connection = DAOUtils.localMySQLConnection("test", "root", "");
+
+        Statement statement = connection.createStatement();
+
+        ResultSet resultet = statement.executeQuery("select * from prova");
+        while (resultet.next()){
+            System.out.println(resultet.getInt(1) + " " + resultet.getString(2));
+        }
+        connection.close();
+    }
+}
--- app/src/main/java/data/DAOException.java
+++ app/src/main/java/data/DAOException.java
@@ -1,4 +1,4 @@
-
+package data;
 
 // This is a runtime exception we define to wrap all the exceptions coming from
 // the DAO objects we're going to define.
