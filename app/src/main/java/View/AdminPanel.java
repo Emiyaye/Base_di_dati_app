dacdc09ba84fc4f4696801a1826f6b3492befbf5
Botti Bao <botti.bao@studio.unibo.it>
2024-06-18 00:30:40 +0200

    Add Panels for admin and user

---
+++ app/src/main/java/View/AdminPanel.java
@@ -0,0 +1,14 @@
+package View;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class AdminPanel extends JPanel {
+
+    public AdminPanel() {
+
+    }
+}
---
+++ app/src/main/java/View/UserPanel.java
@@ -0,0 +1,13 @@
+package View;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JPanel;
+
+public class UserPanel extends JPanel {
+
+    public UserPanel() {
+    }
+
+}
